a0ad194a16574b99f13bdd53527e31c96256d472
curriefavor <lcurrie@ictars.org>
2023-03-13 18:13:41 -0500

    updated

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from './../../models/todo';
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  todos!: Todo[];
+  constructor() {}
+  ngOnInit(): void {
+    this .todos = [
+      {content:'first content',
+    completed: true},
+  {content: 'second content',
+completed: true}]}
+    
+  }
+
+
